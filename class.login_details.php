47a6e1c71a08ad3b306849ced629fad134895da6
davidpalacin <55503430+davidpalacin@users.noreply.github.com>
2020-04-30 19:01:31 +0200

    Add files via upload

---
+++ class.login_details.php
@@ -0,0 +1,24 @@
+<?php
+class Login_Details extends DataBoundObject {
+
+        protected $login_details;
+        protected $user_id;
+        protected $last_activity;
+        protected $is_type;
+
+        protected function DefineTableName() {
+                return("login_details");
+        }
+
+        protected function DefineRelationMap() {
+                return(array(
+                        "login_details" => "login_details",
+                        "user_id" => "user_id",
+                        "last_activity" => "last_activity",
+                        "is_type" => "is_type"  
+                ));
+        }
+
+}
+
+?>
